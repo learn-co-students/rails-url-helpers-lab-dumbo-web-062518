aefa6811e618f0fd07b72abc809afb6f96b2bee6
github username <github email address>
2018-07-20 17:15:01 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,26 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = set_student
+    student_path(@student)
   end
+
+  def activate
+    student = Student.find(params[:id])
+    student.update(active: !student.active)
+    redirect_to student_path(student)
+  end
+
+  # def patch
+  #   student = set_student
+  #   student.update(active: !student.active)
+  #   student_path(student)
+  # end
 
   private
 
---
+++ db/migrate/20180720190455_add_active_fieldname_to_students.rb
@@ -0,0 +1,5 @@
+class AddActiveFieldnameToStudents < ActiveRecord::Migration
+  def change
+    add_column :students, :active, :boolean, default: false
+  end
+end
