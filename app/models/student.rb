c9dee4bebba6a0167a58a66e9b4ac4d90fb2b53b
M01-M01 <marlonsaddress@gmail.com>
2018-07-24 11:02:07 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,20 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active?
+    if self.active == false
+      "inactive"
+    else
+      "active"
+    end
+  end
+
+  def reactive
+    if self.active == true
+      "inactive"
+    else
+      "active"
+    end
+  end
 end
